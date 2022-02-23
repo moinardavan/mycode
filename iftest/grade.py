e71f5d01f77ae2a6e8cdbba85280ead61f33ffdc
Moinardavan <moinardavan@users.noreply.github.com>
2022-02-23 15:23:09 +0000

    While looping

---
+++ iftest/grade.py
@@ -0,0 +1,8 @@
+d=["F","F","F","F","F","F","D","C","B","A","A"]
+while True:
+    n=int(input())
+    if n<=100 and n>-1:
+        print(d[int(n/10)])
+    else:
+        print("Grade:Outstading")
+
--- iftest/library.py
+++ iftest/library.py
@@ -15,5 +15,6 @@ if user_input > len(books):
     print("Please pick number from the given list")
 else:
     print("You Picked:",books[user_input-1],",Great choice!!")
-
+if user_input == 1:
+    print("Adaptation of the first of J.K. Rowling's popular children's novels about Harry Potter, a boy who learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own. He is summoned from his life as an unwanted child to become a student at Hogwarts, an English boarding school for wizards. There, he meets several friends who become his closest allies and help him discover the truth about his parents' mysterious deaths")
 print("*****************THNAK YOU FOR SHOPPING WITH US*******************")
